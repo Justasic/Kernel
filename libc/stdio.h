bf168e62e1cf278d0e7be7664a228dc8def62915
lordofsraam <lordofsraam@hotmail.com>
2013-07-03 21:07:02 -0400

    Forgot to iterate str (fixed)
    Should I make it *(str[i])? Dunno

--- libc/stdio.h
+++ libc/stdio.h
@@ -4,5 +4,5 @@ unsigned short *vidmem = (unsigned short *) 0xB8000;
 void
 vidmemwrite(const char* str) {
   unsigned i;
-  for (i = 0; str[i] != NULL; ++i) vidmem[i] = (unsigned char) *str | 0x0700;
+  for (i = 0; str[i] != NULL; ++i) vidmem[i] = (unsigned char) *str[i] | 0x0700; //What's 0x0700?
 }
